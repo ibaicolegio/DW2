355c7913c770a085c5c0f1edad25ee71b9e4e99b
ibaicolegio <77358044+ibaicolegio@users.noreply.github.com>
2021-09-20 13:53:24 +0200

    20/09/2021 13:53

--- DEWS - Akaitz Gallastegi/NetBeans/index.php
+++ DEWS - Akaitz Gallastegi/NetBeans/index.php
@@ -4,12 +4,8 @@
         <title></title>
     </head>
     <body>
-        <a href="01.php">link 1</a><br>
-        <a href="02.php">link 2</a><br>
-        <a href="03.php">link 3</a><br>
-        <a href="04.php">link 4</a><br>
-        <a href="05.php?nombre=Maligno">link 5</a><br>
-        <a href="06.php">link 6</a><br>
-        <a href="07.php">link 7</a><br>
+        <h1>Temas</h1>
+        <a href="Tema 1/index.php">Tema 1</a><br>
+        <a href="">Tema 2</a><br>
     </body>
 </html>
