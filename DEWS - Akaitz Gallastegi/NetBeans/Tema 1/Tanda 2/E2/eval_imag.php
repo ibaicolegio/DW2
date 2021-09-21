b2659708fbcbfa42ae70a690be9e4d71aaa79b67
ibaicolegio <77358044+ibaicolegio@users.noreply.github.com>
2021-09-21 11:58:43 +0200

    21/09/2021 11:58

--- DEWS - Akaitz Gallastegi/NetBeans/Tema 1/Tanda 2/E2/eval_imag.php
+++ DEWS - Akaitz Gallastegi/NetBeans/Tema 1/Tanda 2/E2/eval_imag.php
@@ -7,34 +7,46 @@
         <?php
             $cantidad=$_POST["cantidad"];
             $directorio= opendir("DIRIMG");
-            $cant=0;
-            $cantImg=0;
-            $agregados=array();
-            while ($cant<$cantidad && $nombre=readdir($directorio)) {
-                if ($nombre!="." && $nombre!="..") {
-                    array_push($agregados,$nombre);
-                    $cantImg++;
-                }
-            }
-            $numeros=array();
-            while ($cant< $cantImg) {
-                do{
-                    $numero= random_int(0, $cantImg-1);
-                }while(in_array($numero, $numeros));
-                array_push($numeros, $numero);
-                $cant++;
-            }
-            $cant=0;
             
+            $agregados=rutas_imag($directorio);
+            print_r($agregados);
+            //$cantidad=array(array_rands($rutas, $cantidad));
+            
+            function rutas_imag($carpeta) {
+                $cant=0;
+                $agregados=array();
+                $arrext=array("jpg","png","tiff");
+                while ($nombre=readdir($carpeta)) {
+                    $separado=explode(".", $nombre);
+                    $extension=$separado[1];
+                    if (in_array($extension, $arrext)) {
+                        array_push($agregados,$nombre);
+                    }
+                }
+                return $agregados;
+            }
+/*
+            function array_rands($array, $cantidad) {
+                $arrayNuevo=array();
+                $numerosRandom=array();
+                for ($i = 0; $i < $cantidad; $i++) {
+                    do{
+                        $numero= random_int(0,cant($array));
+                    }while(in_array($numero, $numeroRandom));
+                    array_push($arrayNuevo, $array[$numero]);
+                }
+                return $arrayNuevo;
+            }        */
         ?>
         <form name="form" action="eval_imag.php" method="POST">
             <table>
                 <?php
+                $cant=0;
                 while ($cant<$cantidad) {
                     echo "<tr>"
-                            . "<td><img src='DIRIMG/".$agregados[$numeros[$cant]]."' width='100px' heigth='100px'></td>"
-                            . "<td><input type='checkbox' name='Me gusta' id=".$agregados[$numeros[$cant]]." value=".$agregados[$numeros[$cant]]."/></td>"
-                            . "<td><label for=".$agregados[$numeros[$cant]].">Me gusta</label></td>"
+                            . "<td><img src='DIRIMG/".$agregados[$cant]."' width='100px' heigth='100px'></td>"
+                            . "<td><input type='checkbox' name='Me gusta' id=".$agregados[$cant]." value=".$agregados[$cant]."/></td>"
+                            . "<td><label for=".$agregados[$cant].">Me gusta</label></td>"
                         . "</tr>";
                     $cant++;
                 }      
