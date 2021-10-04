187b29c3736b2291d4611c586d007cd38cf79cbc
ibaicolegio <77358044+ibaicolegio@users.noreply.github.com>
2021-10-04 11:02:34 +0200

    04/10/2021 11:02

--- DEWC - Txema Serrano/Tema 3/js/e7.js
+++ DEWC - Txema Serrano/Tema 3/js/e7.js
@@ -1,16 +1,42 @@
+var provincias = new Array("Araba", "Bizkaia", "Gipuzkoa", "Nafarroa", "Lapurdi", "Zuberoa", "Nafarroa Beherea")
+
 var municipios_1 = new Array("Seleccione Municipio", "Vitoria-Gasteiz", "Amurrio", "El Ciego", "La Guardia");
 var municipios_2 = new Array("Seleccione Municipio", "Bilbao", "Barakaldo", "Durango", "Elorrio", "Muzkiz");
 var municipios_3 = new Array("Seleccione Municipio", "Donostia-San Sebastián", "Arrasate-Mondragón", "Eibar");
+var municipios_4 = new Array("Seleccione Municipio", "Iruña", "Burlada", "Estella-Lizarra", "Tafalla");
+var municipios_5 = new Array("Seleccione Municipio", "Baiona", "Bastida", "Hendaya", "Miarritze");
+var municipios_6 = new Array("Seleccione Municipio", "Maule", "Barkoxe", "Sohüta");
+var municipios_7 = new Array("Seleccione Municipio", "Baigorri", "Garazi", "Oztibarre");
 
-var municipios = new Array([],municipios_1,municipios_2,municipios_3);
+var municipios = [
+    [],
+    municipios_1,
+    municipios_2,
+    municipios_3,
+    municipios_4,
+    municipios_5,
+    municipios_6,
+    municipios_7,
+];
+
+function cargar() {
+    for (let i = 0; i < provincias.length; i++) {
+        var opcion=document.createElement("option");
+        opcion.setAttribute("value",i);
+        var nodo=document.createTextNode(provincias[i]);
+        opcion.appendChild(nodo);
+        document.getElementById("provincia").appendChild(opcion);
+    }
+}
+
 function cambiarProvincia() {
     var provincias=document.getElementById("provincia");
     var provinciaIndice=provincias.selectedIndex;
     var provinciaSelecionada=provincias.options[provinciaIndice].text;
     var municipio=municipios[provinciaIndice];
     document.getElementById("municipio").innerHTML = "";
-    if (provinciaSelecionada!=0) {
-        for (let i = 0; i < municipios.length; i++) {
+    if (provinciaIndice!==0) {
+        for (let i = 0; i < municipio.length; i++) {
             var opcion=document.createElement("option");
             opcion.setAttribute("value",i);
             var nodo=document.createTextNode(municipio[i]);
