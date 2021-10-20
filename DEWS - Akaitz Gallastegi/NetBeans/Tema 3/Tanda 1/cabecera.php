e246568b2f2de52ba2bd9431f26d92ad775857fc
ibaicolegio <77358044+ibaicolegio@users.noreply.github.com>
2021-10-20 11:57:16 +0200

    20/10/2021 11:57

---
+++ DEWS - Akaitz Gallastegi/NetBeans/Tema 3/Tanda 1/cabecera.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title><?php echo $config_forumsname; ?></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="stylesheet.css" type="text/css" />
+    </head>
+    <body>
+        <div id="header">
+            <h1>SUBASTAS CIUDAD JARDIN</h1>
+        </div>
+        <div id="menu">
+            <a href="index.php">Home</a>
+            <?php
+            if (isset($_SESSION['USERNAME']) == TRUE) {
+                echo "<a href='logout.php'>Logout</a>";
+            } else {
+                echo "<a href='login.php'>Login</a>";
+            }
+            ?>
+            <a href="newitem.php">New Item</a>
+        </div>
+        <div id="container">
+            <div id="bar">
+                <?php require("bar.php"); ?>
+            </div>
+            <div id="main">
