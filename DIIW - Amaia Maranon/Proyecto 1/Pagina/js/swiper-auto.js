26ab603ad4ad63c7bbb32bee4c47473c2908a4bc
ibaicolegio <77358044+ibaicolegio@users.noreply.github.com>
2021-11-18 12:01:00 +0100

    18/11/2021 12:00

--- DIIW - Amaia Maranon/Proyecto 1/Pagina/js/swiper-auto.js
+++ DIIW - Amaia Maranon/Proyecto 1/Pagina/js/swiper-auto.js
@@ -5,3 +5,10 @@ var swiper = new Swiper('.mySwiper', {
       disableOnInteraction: true,
   },
   });
+var swiper2 = new Swiper('.mySwiper2', {
+    loop: true,
+    autoplay: {
+      delay: 10000,
+      disableOnInteraction: true,
+  },
+  });
