03469fd5ad92d4bcb6f450a0a41a34c02d888b33
ibaicolegio <77358044+ibaicolegio@users.noreply.github.com>
2021-11-18 10:07:45 +0100

    18/11/2021 10:07

---
+++ DIIW - Amaia Maranon/Proyecto 1/Pagina/js/js.js
@@ -0,0 +1,7 @@
+function encender(){
+    if(document.getElementById("contacto").style.display=="block"){
+        document.getElementById("contacto").style.display="none";
+    } else {
+        document.getElementById("contacto").style.display="block";
+    }
+}
