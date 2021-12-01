da6b6455061468ad132317153a38effb8c3d35ac
ibaicolegio <77358044+ibaicolegio@users.noreply.github.com>
2021-12-01 09:59:35 +0100

    01/12/2021 9:59

--- DEWC - Txema Serrano/DEWC.bat
+++ DEWC - Txema Serrano/DEWC.bat
@@ -1,2 +1,6 @@
-xcopy /E /Y "C:\Users\dw2\Documents\GitHub\DW2\DEWC - Txema Serrano\2ª Evaluacion" "C:\wamp64\www\DEWC"
+rmdir /Q /S "C:\wamp64\www\DEWC"
+mkdir "C:\wamp64\www\DEWC"
+xcopy /E /Y "C:\Users\dw2\Documents\GitHub\DW2\DEWC - Txema Serrano\2.Evaluacion" "C:\wamp64\www\DEWC"
 start http://localhost/DEWC/
+
+
