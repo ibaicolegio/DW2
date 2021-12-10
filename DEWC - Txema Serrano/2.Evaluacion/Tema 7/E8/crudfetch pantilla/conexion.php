56a28bccf8e471af04e61b52c1e30a61f5b683b9
ibaicolegio <77358044+ibaicolegio@users.noreply.github.com>
2021-12-10 09:59:53 +0100

    10/12/2021 9:59

---
+++ DEWC - Txema Serrano/2.Evaluacion/Tema 7/E8/crudfetch pantilla/conexion.php
@@ -0,0 +1,12 @@
+<?php
+    $servidor = "mysql:dbname=productos;host=localhost";
+    $user = "dw2";
+    $pass = "dw2";
+    try {
+        $pdo = new PDO($servidor, $user, $pass,
+         array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    } catch (PDOException $e) {
+        echo "conexion fallida" .$e->getMessage();
+    }
+
+?>
